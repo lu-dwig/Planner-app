525def8edfb16c7d35c08a13fa30b1e7eac41753
lu-dwig <murimiludwig@gmail.com>
2023-07-04 21:29:58 +0300

    creation of the timer widget

---
+++ planner-app/src/widgets/TimerWidgt.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+ 
+export default function TimerWidget() {
+    const [timerListData, setTimerListData] = useState({
+        time: 0,
+        isRunning: false,
+    })
+  
+  
+  
+}
