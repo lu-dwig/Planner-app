586e8da752138132af182742bc2dfdaf551d3ab7
lu-dwig <murimiludwig@gmail.com>
2023-07-04 21:19:03 +0300

    creation of the reminder functions in the reminder widget widget

--- planner-app/src/widgets/ReminderWidget.js
+++ planner-app/src/widgets/ReminderWidget.js
@@ -39,7 +39,44 @@ export default function ReminderListWidget() {
                 >+ New</button>
             </div>
             {reminderListData?.length > 0 && reminderListData.map((reminder, index) => {
-            
+            return (
+                <div className='reminder-container' key={index}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "row" }}>
+                        <input className='text-input' value={reminder.title}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    let reminderList = [...reminderListData]
+                                    reminderList[index].title = e.target.value
+                                    reminderList[index].datestamp = new Date()
+                                    setReminderListData([...reminderList, {
+                                        title: '',
+                                        datestamp: '',
+                                    }])
+                                }
+                            }}
+                            onChange={(e) => {
+                                let reminderList = [...reminderListData]
+                                reminderList[index].title = e.target.value
+                                reminderList[index].datestamp = new Date()
+                                setReminderListData(reminderList)
+                            }} />
+                        <button
+                            style={{
+                                background: "none",
+                                border: "none",
+                                color: "white",
+                                fontSize: "15px",
+                                cursor: "pointer",
+                            }}
+                            onClick={() => {
+                                let reminderList = [...reminderListData]
+                                reminderList.splice(index, 1)
+                                setReminderListData(reminderList)
+                            }}
+                        >X</button>
+                    </div>
+                </div>
+            )
             })}
         </div>
     ) 
