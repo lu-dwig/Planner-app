e5b1d0599a27c80f60c24adcec8edb120af8b6f2
lu-dwig <murimiludwig@gmail.com>
2023-07-05 22:06:55 +0300

    state of the app

--- planner-app/src/App.js
+++ planner-app/src/App.js
@@ -12,6 +12,16 @@ function App() {
         { id: new Date().getTime() + 2, component: <CalendarWidget />, area: 'right-widget', name: "Calendar" },
         { id: new Date().getTime() + 1, component: <TimerWidget />, area: 'left-widget', name:"Timer" },
     ])
+    const [showWidgetModal, setShowWidgetModal] = useState(false)
+    const [selectedWidgetArea, setSelectedWidgetArea] = useState('')
+    
+    const removeWidget = (index) => {
+        try {
+            setWidgets(widgets.filter(widget => widget.id !== index))
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     return (
         <>
