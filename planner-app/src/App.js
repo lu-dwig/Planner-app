b8f1c33e523d1b8938986249833cefd6067d3165
lu-dwig <murimiludwig@gmail.com>
2023-07-05 22:06:26 +0300

    routing of the planner app at the app.js file

--- planner-app/src/App.js
+++ planner-app/src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
-
+import ClockWidget from './widget/ClockWidget';
+import WidgetGalleryModal from './modals/WidgetGalleryModal';
+import Draggable from 'react-draggable';
+import TimerWidget from './widget/TimerWidget';
+import CalendarWidget from './widget/CalendarWidget';
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
-    </div>
-  );
+    const [widgets, setWidgets] = useState([
+        { id: new Date().getTime(), component: <ClockWidget />, area: 'main-widget', name: "Date and Time" },
+        { id: new Date().getTime() + 2, component: <CalendarWidget />, area: 'right-widget', name: "Calendar" },
+        { id: new Date().getTime() + 1, component: <TimerWidget />, area: 'left-widget', name:"Timer" },
+    ])
+    
+    return (
+        <>
+            
+        </>
+    )
 }
-
+ 
 export default App;
--- planner-app/src/modals/WidgetGalleryModal.js
+++ planner-app/src/modals/WidgetGalleryModal.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import ClockWidget from '../widgets/ClockWidget'
-import ReminderListWidget from '../widgets/ReminderListWidget'
-import TimerWidget from '../widgets/TimerWidget'
-import CalendarWidget from '../widgets/CalendarWidget'
+import ClockWidget from '../widget/ClockWidget'
+import ReminderListWidget from '../widget/ReminderListWidget'
+import TimerWidget from '../widget/TimerWidget'
+import CalendarWidget from '../widget/CalendarWidget'
 
 export default function WidgetGalleryModal({ setShowWidgetModal, selectedWidgetArea, widgets, setWidgets }) {
-    const [galleryWidgets, setGalleryWidgets] = useState([
+    const [galleryWidgets] = useState([
         { component: <ClockWidget />, name: "Date and Time" },
         { component: <ReminderListWidget />, name: "Reminder List" },
         { component: <TimerWidget />, name: "Timer" },
